1135ec3e9a4b693a55e5700bf2096611510e08d5
Edward Tomasz Napierała <trasz@freebsd.org>
2008-05-13 09:40:32 +0000

    fix spelling in copyright.
    git-svn-id: https://libsmf.svn.sourceforge.net/svnroot/libsmf/trunk@497 bede6af4-a197-dd11-9785-00d0b7b2942b

--- src/smfsh.c
+++ src/smfsh.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2007, 2008 Edward Tomasz Napierała <trasz@FreeBSD.org>.
+ * Copyright (c) 2007, 2008 Edward Tomasz Napierała <trasz@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
